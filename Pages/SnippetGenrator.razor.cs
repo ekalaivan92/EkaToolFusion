d6f6045841d7c758dc766940c526c08372ec1d4d
kalai <kalaiekalai@outlook.com>
2024-05-18 19:59:48 +0530

    feat: add initial project files with VS Snippet generator

---
+++ Pages/SnippetGenrator.razor.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Components;
+
+namespace EkaToolFusion.Pages;
+
+public partial class SnippetGenrator : ComponentBase
+{
+    public string CodeSnippet { get; set; } = "<kalai>test</kalai>";
+}
